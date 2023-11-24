c09aa8249777afca740ee39e00b06e0cf8e029ec
Suntiparb <pae@Suntiparbs-MacBook-Air.local>
2023-11-24 19:54:57 +0700

    chore(preact): add react package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import partytown from "@astrojs/partytown";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,6 @@ export default defineConfig({
     config: {
       forward: ['dataLayer.push']
     }
-  })],
+  }), preact()],
   site: "https://elaborate-panda-f33b08.netlify.app"
 });
